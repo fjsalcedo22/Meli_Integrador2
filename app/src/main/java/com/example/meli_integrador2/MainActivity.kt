ee59be5fd85b3cf6c21089a188bb2e00f82afbe0
fsalcedo <fernando.salcedo@mercadolibre.com.co>
2022-06-07 10:22:54 -0500

    add funcionality to home screen

--- app/src/main/java/com/example/meli_integrador2/MainActivity.kt
+++ app/src/main/java/com/example/meli_integrador2/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.meli_integrador2
 
+import android.content.Intent
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.example.meli_integrador2.databinding.ActivityMainBinding
+import com.example.meli_integrador2.utils.TextChangedListener
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        binding.buttonStart.setOnClickListener {
+            val intentToScreenActivities = Intent(this, Activities::class.java)
+            intentToScreenActivities.putExtra("", "") //TODO look at the putExtra
+            startActivity(intentToScreenActivities)
+        }
+
+        binding.textViewTermsAndConditions.setOnClickListener {
+           // binding.viewTermsAndConditions.visibility = View.VISIBLE
+        }
+
+        binding.editTextParticipants.addTextChangedListener(TextChangedListener(binding.buttonStart, binding.editTextParticipants, this))
     }
 }
